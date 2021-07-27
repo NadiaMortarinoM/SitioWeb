104a5e8adef0b7c4b4048efc3fd626ad89de68ba
NadiaMortarinoM <nmortarino@gmail.com>
2021-07-27 16:21:01 -0300

    Subiendo archivos del sitioweb

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<?php include("template/cabecera.php"); ?>
+
+            <div class="jumbotron text-center">
+                <h1 class="display-3">Bienvenidos al sitio web de libros:</h1>
+                <p class="lead">Consulta libro de programación, biología, etc</p>
+                <hr class="my-2">
+                <img width="150" src="img/Tulipan.jpg" class="img-thumbnail rounded mx-auto d-block">
+                <p>Más información</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver almacén de libros</a>
+                </p>
+            </div>
+            
+<?php include("template/pie.php"); ?>
+
+        
---
+++ productos.php
@@ -0,0 +1,23 @@
+<?php include("template/cabecera.php"); ?>
+
+
+<?php include("administrador/config/bd.php");
+$sentenciaSQL=$conexion->prepare("SELECT * FROM libros");
+$sentenciaSQL->execute();
+$listaLibros=$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php foreach($listaLibros as $libro) { ?>
+
+    <div class="col-md-3">
+    <div class="card">
+    <img class="card-img-top" src="./img/<?php $libro['imagen']; ?>" alt="">
+    <div class="card-body">
+    <h4 class="card-title"><?php $libro['nombre']; ?></h4>
+    <a name="" id="" class="btn btn-primary" href="https://goalkicker.com" role="button"> Ver más </a>
+    </div>
+    </div>  
+    </div>
+<?php } ?>
+   
+<?php include("template/pie.php"); ?>
